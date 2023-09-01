a88e20842ff2a1a5c7d406c3b55f90aa99c3efc5
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-02 01:10:17 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -31,7 +31,7 @@ foreach ($jsonFile in $jsonFiles) {
     Write-Host "KVM Name: $kvmName"
 
     $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-    $headers.Add("Authorization", "Bearer ya29.a0AfB_byBVRRWv3j120nTXVXah3wvqwIpBIGNguhajsUiE87R496vX-zJEJrL3ZLCcGyBkxL5Ynuu61HEwz9nn6_evRSewYFWQT3gi4mrocvzdmo8odRtPVYeFMgXVk7lerSLLom8dDkKCWJCRcNq7yKKM_kIgl_JUZv_USetE1bmNWUwaCgYKAZQSARESFQHsvYlsDh3J5BDbCEj488voROYSPA0182")
+    $headers.Add("Authorization", "Bearer $token")
     $headers.Add("Content-Type", "application/json")
 
     $body = @"
