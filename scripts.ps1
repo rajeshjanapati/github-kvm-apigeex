5ec0bb8047a5e51123c2ab529c9fdb7af22c8688
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-01 16:02:40 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -18,30 +18,48 @@ cd kvms
 $jsonFiles = Get-ChildItem -Filter *.json -Recurse
 
 # Loop through each JSON file and make POST requests
-foreach ($jsonFile in $jsonFiles) {
-    Write-Host "entered into foreach..."
-    $jsonContent = Get-Content -Path $jsonFile -Raw
-    # $apiUrl = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
-    $headers = @{
-        "Authorization" = "Bearer $token"
-        "Content-Type" = "application/json"
-    }
-    $kvmData = $jsonContent | ConvertFrom-Json
-    Write-Host $kvmData
-    foreach ($entry in $kvmData) {
-        $key = $entry.Key
-        $value = $entry.Value
+# foreach ($jsonFile in $jsonFiles) {
+#     Write-Host "entered into foreach..."
+#     $jsonContent = Get-Content -Path $jsonFile -Raw
+#     # $apiUrl = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
+#     $headers = @{
+#         "Authorization" = "Bearer $token"
+#         "Content-Type" = "application/json"
+#     }
+#     $kvmData = $jsonContent | ConvertFrom-Json
+#     Write-Host $kvmData
+#     foreach ($entry in $kvmData) {
+#         $key = $entry.Key
+#         $value = $entry.Value
     
-        $kvmEntry = @{
-            "name" = $key
-            "encrypted" = "false"
-            "entry" = $value
-        } | ConvertTo-Json
-    $baseUrl = "https://api.enterprise.apigee.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
+#         $kvmEntry = @{
+#             "name" = $key
+#             "encrypted" = "false"
+#             "entry" = $value
+#         } | ConvertTo-Json
+#     $baseUrl = "https://api.enterprise.apigee.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
 
-    $kvmUrl = $baseUrl+$key+"/entries"
-    Invoke-RestMethod -Uri $kvmUrl -Method Post -Headers $headers -Body $kvmEntry
-}
+#     $kvmUrl = $baseUrl+$key+"/entries"
+#     Invoke-RestMethod -Uri $kvmUrl -Method Post -Headers $headers -Body $kvmEntry
+# }
     
+# }
+
+# Define the API endpoint
+$apiUrl = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/github-te/entries"
+
+# Define the KVM entry data (key and value)
+$kvmData = @{
+    "key" = "github-test"
+    "value" = "1234"
+} | ConvertTo-Json
+
+# Set up the request headers
+$headers = @{
+    "Authorization" = "Bearer $token"
+    "Content-Type" = "application/json"
 }
-    
+
+# Make the POST request to create the KVM entry
+$response = Invoke-RestMethod -Uri $apiUrl -Method Post -Headers $headers -Body $kvmData
+
