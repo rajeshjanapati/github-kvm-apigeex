8896fcb7f83b12d6ba68a7f2d68d02c12f810d35
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-01 16:46:12 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -18,14 +18,16 @@ cd kvms
 $jsonFiles = Get-ChildItem -Filter *.json -Recurse
 
 # Loop through each JSON file and make POST requests
-# foreach ($jsonFile in $jsonFiles) {
-#     Write-Host "entered into foreach..."
-#     $jsonContent = Get-Content -Path $jsonFile -Raw
-#     # $apiUrl = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
-#     $headers = @{
-#         "Authorization" = "Bearer $token"
-#         "Content-Type" = "application/json"
-#     }
+foreach ($jsonFile in $jsonFiles) {
+    Write-Host "entered into foreach..."
+    $jsonContent = Get-Content -Path $jsonFile -Raw
+    $apiUrl = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
+    $headers = @{
+        "Authorization" = "Bearer $token"
+        "Content-Type" = "application/json"
+    }
+    Write-Host $jsonContent
+
 #     $kvmData = $jsonContent | ConvertFrom-Json
 #     Write-Host $kvmData
 #     foreach ($entry in $kvmData) {
@@ -43,19 +45,19 @@ $jsonFiles = Get-ChildItem -Filter *.json -Recurse
 #     Invoke-RestMethod -Uri $kvmUrl -Method Post -Headers $headers -Body $kvmEntry
 # }
     
-# }
-
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", "Bearer ya29.a0AfB_byDVwfNZVNiTBsatmu7gJEQyPExZ3TBhYONcediO5NcqjC6jf1o34DmhvKWHc999CUnVFJfjfjkELG3OFRGebsOAPMvoJmLsRccgc4gbDtwqWfVbrI_1STm9yQhpoxFpnPKLZQY5K0YCu9U0sNZaeRnz31PTu-vWYR5Px7HKYXkaCgYKAaISARESFQHsvYls6LkmPwzCEYsHSFPkVZQVDA0182")
-$headers.Add("Content-Type", "application/json")
-
-$body = @"
-{
-    `"name`":`"test`",
-    `"value`":`"12345`"
 }
-"@
 
-$response = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/github-KVM/entries' -Method 'POST' -Headers $headers -Body $body
-$response | ConvertTo-Json
+# $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+# $headers.Add("Authorization", "Bearer ya29.a0AfB_byDVwfNZVNiTBsatmu7gJEQyPExZ3TBhYONcediO5NcqjC6jf1o34DmhvKWHc999CUnVFJfjfjkELG3OFRGebsOAPMvoJmLsRccgc4gbDtwqWfVbrI_1STm9yQhpoxFpnPKLZQY5K0YCu9U0sNZaeRnz31PTu-vWYR5Px7HKYXkaCgYKAaISARESFQHsvYls6LkmPwzCEYsHSFPkVZQVDA0182")
+# $headers.Add("Content-Type", "application/json")
+
+# $body = @"
+# {
+#     `"name`":`"test`",
+#     `"value`":`"12345`"
+# }
+# "@
+
+# $response = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/github-KVM/entries' -Method 'POST' -Headers $headers -Body $body
+# $response | ConvertTo-Json
 
