043bd1f73a00e73259827b5ef7d2fc66cad0a61b
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-01 15:25:29 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -19,13 +19,14 @@ $jsonFiles = Get-ChildItem -Filter *.json -Recurse
 
 # Loop through each JSON file and make POST requests
 foreach ($jsonFile in $jsonFiles) {
+    Write-Host "entered into foreach..."
     $jsonContent = Get-Content -Path $jsonFile.FullName -Raw
     $apiUrl = "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps"
     $headers = @{
         "Authorization" = "Bearer $token"
         "Content-Type" = "application/json"
     }
-
+    Write-Host $jsonContent
     $response = Invoke-RestMethod -Uri $apiUrl -Method:Post -Headers $headers -Body $jsonContent
 
     Write-Host "File $($jsonFile.Name) uploaded. Response: $($response | ConvertTo-Json -Depth 2)"
