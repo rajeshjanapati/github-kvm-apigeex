285fa88fbb3ee94aadfee7cd7a9db7584fc5c4b6
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-03 11:53:35 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -50,17 +50,17 @@ foreach ($jsonFile in $jsonFiles) {
     $entries = $jsonObject.entry
     Write-Host $entries
 
-    foreach ($entry in $entries) {
-        $name = $entry.key
-        $value = $entry.value
-        Write-Host "Name: $name, Value: $value"
-        $body2 =@{
-        "name"=$name;
-        "value"=$value;
-        }
-        $entryObject = @{
+    foreach ($entry in $jsonObject.entry) {
+    $name = $entry.key
+    $value = $entry.value
+    Write-Host "Name: $name, Value: $value"
+    $body2 = @{
+        "name" = $name
+        "value" = $value
+    }
+    $entryObject = @{
         "entry" = $body2
-        }
+    }
         
         Write-Host $body2
         # $kvmentry = Invoke-RestMethod "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/$kvmName/entries" -Method 'POST' -Headers $headers -Body ($entryObject|ConvertTo-Json)
