a496bb48b678a1a12316b5ecfef45e426ce57151
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-03 23:48:52 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -38,39 +38,43 @@ foreach ($jsonFile in $jsonFiles) {
     $kvmget | ConvertTo-Json
     Write-Host $kvmget
 
+    $kvmgetentries = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps' -Method 'GET' -Headers $headers
+    $kvmgetentries | ConvertTo-Json
+    Write-Host $kvmgetentries
+
     # Your array
     $array = $kvmget
     
-    foreach ($valueToCheck in $array) {
-        if ($array -contains $valueToCheck) {
-            Write-Host "$valueToCheck is present in the array."
-            $entries = $jsonData.entry
-            Write-Host "Values: $vlaues"
+    # foreach ($valueToCheck in $array) {
+    #     if ($array -contains $valueToCheck) {
+    #         Write-Host "$valueToCheck is present in the array."
+    #         $entries = $jsonData.entry
+    #         Write-Host "Values: $vlaues"
         
-            foreach ($entry in $entries) {
-                Write-Host "step-2"
-                $name = $entry.key
-                $value = $entry.value
-                Write-Host "Key: $name, Value: $value"
-                $body2 = @{
-                    "name" = $name;
-                    "value" = $value;
-                }
-                Write-Host "body2: $body2"
+    #         foreach ($entry in $entries) {
+    #             Write-Host "step-2"
+    #             $name = $entry.key
+    #             $value = $entry.value
+    #             Write-Host "Key: $name, Value: $value"
+    #             $body2 = @{
+    #                 "name" = $name;
+    #                 "value" = $value;
+    #             }
+    #             Write-Host "body2: $body2"
                 
-                $kvmentry = Invoke-RestMethod "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/$kvmName/entries" -Method 'POST' -Headers $headers -Body ($body2|ConvertTo-Json)
-                $kvmentry | ConvertTo-Json
-            }
-        } else {
-            $body1 =@{
-                "name"=$kvmName;
-                "encrypted"=true;
-                }
-            Write-Host $body1
-            Write-Host "$valueToCheck is not present in the array."
-            $kvmcreate = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps' -Method 'POST' -Headers $headers -Body ($body1|ConvertTo-Json)
-            $kvmcreate | ConvertTo-Json
-        }
-    }
+    #             $kvmentry = Invoke-RestMethod "https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps/$kvmName/entries" -Method 'POST' -Headers $headers -Body ($body2|ConvertTo-Json)
+    #             $kvmentry | ConvertTo-Json
+    #         }
+    #     } else {
+    #         $body1 =@{
+    #             "name"=$kvmName;
+    #             "encrypted"=true;
+    #             }
+    #         Write-Host $body1
+    #         Write-Host "$valueToCheck is not present in the array."
+    #         $kvmcreate = Invoke-RestMethod 'https://apigee.googleapis.com/v1/organizations/esi-apigee-x-394004/environments/eval/keyvaluemaps' -Method 'POST' -Headers $headers -Body ($body1|ConvertTo-Json)
+    #         $kvmcreate | ConvertTo-Json
+    #     }
+    # }
     
     }
